7e55277e5383b08cad680a51ed94375f53011021
titouanpastor <titouan.pastor@gmail.com>
2023-03-15 15:32:46 +0100

    creating a small client (login & main blog page)

--- client/view/blogClient.php
+++ client/view/blogClient.php
@@ -5,59 +5,70 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../css/auth.css">
+    <link rel="stylesheet" href="../css/blog.css">
     <title>Blog Communautaire</title>
 </head>
 
 <body>
-    <main>
+    <header>
         <h1>Blog Communautaire</h1>
-        <h2>Articles</h2>
-        <div id="articles"></div>
-        <h2>Commentaires</h2>
-        <div id="comments"></div>
-        <h2>Formulaire d'ajout d'article</h2>
-        <form id="formArticle">
-            <label for="title">Titre</label>
-            <input type="text" name="title" id="title">
-            <label for="content">Contenu</label>
-            <textarea name="content" id="content" cols="30" rows="10"></textarea>
-            <input type="submit" value="Envoyer">
-        </form>
-        <h2>Formulaire d'ajout de commentaire</h2>
-        <form id="formComment">
-            <label for="content">Contenu</label>
-            <textarea name="content" id="content" cols="30" rows="10"></textarea>
-            <input type="submit" value="Envoyer">
-        </form>
+        <nav>
+            <ul>
+                <li><a class="connexion-btn" href="authentificationClient.php">Connexion</a></li>
+            </ul>
+        </nav>
+    </header>
 
+    <main>
+        <!-- Formulaire pour ajouter un nouvel article -->
+        <form>
+            <h2>Nouvel article</h2>
+            <label for="title">Titre :</label>
+            <input type="text" id="title" name="title"><br>
+            <label for="content">Contenu :</label>
+            <textarea id="content" name="content"></textarea><br>
+            <input type="submit" value="Publier">
+        </form>
     </main>
-    
-    <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+
+
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
     <script>
-        // Récupération des articles
+        // Récupération des différents articles grace à l'api
+        let token = localStorage.getItem('token');
         $.ajax({
-            url: 'http://localhost:8080/blog/api/articles',
-            type: 'GET',
-            dataType: 'json',
+            url: "http://localhost/blog/api/articles",
+            type: "GET",
+            dataType: "json",
+            headers: {
+                "Authorization": "Bearer " + token
+            },
             success: function(data) {
                 console.log(data);
-                for (let i = 0; i < data.length; i++) {
-                    $('#articles').append('<div id="article' + data[i].id_article + '"></div>');
-                    $('#article' + data[i].id_article).append('<h3>' + data[i].title + '</h3>');
-                    $('#article' + data[i].id_article).append('<p>' + data[i].content + '</p>');
-                    $('#article' + data[i].id_article).append('<p>Nombre de likes : ' + data[i].nb_like + '</p>');
-                    $('#article' + data[i].id_article).append('<p>Nombre de dislikes : ' + data[i].nb_dislike + '</p>');
-                    $('#article' + data[i].id_article).append('<p>Auteur : ' + data[i].username + '</p>');
-                    $('#article' + data[i].id_article).append('<p>Date de publication : ' + data[i].publication_date + '</p>');
-                    $('#article' + data[i].id_article).append('<p>Heure de publication : ' + data[i].publication_time + '</p>');
-                    $('#article' + data[i].id_article).append('<button id="like' + data[i].id_article + '">Like</button>');
-                    $('#article' + data[i].id_article).append('<button id="dislike' + data[i].id_article + '">Dislike</button>');
-                    $('#article' + data[i].id_article).append('<button id="comment' + data[i].id_article + '">Commenter</button>');
-                    $('#article' + data[i].id_article).append('<div id="comments' + data[i].id_article + '"></div>');
+                // mise en forme de l'article
+                for (let i = 0; i < data.data.length; i++) {
+                    let article = data.data[i];
+                    let date = new Date(article.publication_date);
+                    let dateFormated = date.getDate() + " " + date.toLocaleString('default', { month: 'long' }) + " " + date.getFullYear();
+                    let articleHtml = `
+                    <div class="article">
+                        <h2>${article.title}</h2>
+                        <p>${article.content}</p>
+                        <div class="meta">
+                            <span class="date">${dateFormated}</span>
+                            <span class="author">${article.author}</span>
+                            <span class="likes">${article.likes} like</span>
+                            <span class="dislikes">${article.dislikes} dislike</span>
+                        </div>
+                    </div>
+                    `;
+                    // insertion de l'article dans le main
+                    $('main').append(articleHtml);
                 }
             },
-            error: function(error) {
-                console.log(error);
+            error: function(xhr, status, error) {
+                console.log(xhr);
             }
         });
     </script>
